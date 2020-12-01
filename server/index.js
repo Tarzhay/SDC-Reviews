26cd3f58441819868bcf927d94e5d43e9a419c87
Max Einhorn <maxeinhorn@gmail.com>
2020-11-30 16:29:06 -0800

    About to begin pm2 implementation

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const _ = require('underscore');
 
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, '/../client/dist')));
 
 app.get('/:id', (req, res) => {
   res.sendFile(path.join(__dirname + '/../client/dist/index.html'));
